78e981b229871815a55cab0a612ec127bb2f0586
phyo <moehtat027@gmail.com>
2024-11-12 15:02:58 +0700

    Like or Dislike of photo

--- gs01_flutter/count_app/lib/counter-app.dart
+++ gs01_flutter/count_app/lib/counter-app.dart
@@ -7,21 +7,24 @@ class CounterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu_open),
-        title: Text("Bagan"),
+        leading: const Icon(Icons.menu_open),
+        title: const Text("Bagan"),
         centerTitle: true,
         backgroundColor: Colors.amber[300],
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.settings),
+            onPressed: () {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(content: Text("Setting press")));
+            },
+            icon: const Icon(Icons.settings),
           )
         ],
       ),
       body: Column(
         children: [
           Image(
-            image: AssetImage("images/bagan.jpg"),
+            image: const AssetImage("images/bagan.jpg"),
             width: double.infinity,
             height: MediaQuery.of(context).size.height / 3 * 1,
             fit: BoxFit.cover,
@@ -42,19 +45,40 @@ class CounterBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         const Text(
-          "Stateful Widget",
+          "The Beauty of Bagan",
           style: TextStyle(fontSize: 25),
         ),
-        const Text(
-          "Count is 0",
-          style: TextStyle(fontSize: 23),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Dislike 0",
+              style: TextStyle(fontSize: 23),
+            ),
+            Text(
+              "Like 0",
+              style: TextStyle(fontSize: 23),
+            ),
+          ],
         ),
         SizedBox(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(onPressed: () {}, child: Text("Count Down")),
-              ElevatedButton(onPressed: () {}, child: Text("Count Up")),
+              SizedBox(
+                width: 135,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Dislike"),
+                ),
+              ),
+              SizedBox(
+                width: 135,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Like"),
+                ),
+              ),
             ],
           ),
         )
